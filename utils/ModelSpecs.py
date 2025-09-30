c0cdebccc351922dd18492e65b04b18f710df54d
Arush Bansal <arush4bansal@gmail.com>
2025-09-30 22:13:37 +0530

    0.4

--- utils/ModelSpecs.py
+++ utils/ModelSpecs.py
@@ -4,18 +4,18 @@ from pydantic import BaseModel, Field
 VariantName = Literal["toy", "tiny", "small", "target"]
 
 class ModelSpecs(BaseModel):
-    VOCAB_SIZE: int
-    BLOCK_SIZE: int
+    VOCAB_SIZE: int # total number of tokens
+    BLOCK_SIZE: int # what is the maximum context length for predictions?
     N_EMBD: int
     N_HEAD: int
     N_LAYER: int
     DROPOUT: float = Field(..., ge=0.0, le=1.0)
 
     variants: ClassVar[Dict[str, dict]] = {
-        "toy": dict(VOCAB_SIZE=500, BLOCK_SIZE=64, N_EMBD=128, N_HEAD=4, N_LAYER=3, DROPOUT=0.1),
-        "tiny": dict(VOCAB_SIZE=1000, BLOCK_SIZE=128, N_EMBD=256, N_HEAD=6, N_LAYER=6, DROPOUT=0.15),
-        "small": dict(VOCAB_SIZE=1800, BLOCK_SIZE=128, N_EMBD=384, N_HEAD=6, N_LAYER=6, DROPOUT=0.2),
-        "target": dict(VOCAB_SIZE=3200, BLOCK_SIZE=256, N_EMBD=512, N_HEAD=8, N_LAYER=8, DROPOUT=0.25),
+        "toy": dict(VOCAB_SIZE=50261, BLOCK_SIZE=64, N_EMBD=128, N_HEAD=4, N_LAYER=3, DROPOUT=0.1),
+        "tiny": dict(VOCAB_SIZE=50261, BLOCK_SIZE=128, N_EMBD=256, N_HEAD=6, N_LAYER=6, DROPOUT=0.15),
+        "small": dict(VOCAB_SIZE=50261, BLOCK_SIZE=256, N_EMBD=384, N_HEAD=6, N_LAYER=6, DROPOUT=0.2), # andrej kerpathy model
+        "target": dict(VOCAB_SIZE=50261, BLOCK_SIZE=512, N_EMBD=512, N_HEAD=8, N_LAYER=8, DROPOUT=0.25),
     }
 
     @classmethod
